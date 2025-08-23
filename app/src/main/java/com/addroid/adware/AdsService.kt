8c7334e1aedf9783e41598c2ce614bbe58529f66
IsHacker <ishayu301@gmail.com>
2025-08-23 20:30:37 +0530

    Added AdsService

--- app/src/main/java/com/addroid/adware/AdsService.kt
+++ app/src/main/java/com/addroid/adware/AdsService.kt
@@ -4,10 +4,15 @@ import android.app.Service
 import android.content.Intent
 import android.content.IntentFilter
 import android.os.IBinder
+import android.util.Log
 import com.google.android.gms.ads.MobileAds
+
 
 class AdsService : Service() {
     override fun onBind(p0: Intent?): IBinder? {
+        return null
+    }
+    override fun onCreate() {
         val intentFilter = IntentFilter()
         intentFilter.addAction(Intent.ACTION_PACKAGE_ADDED)
         intentFilter.addAction(Intent.ACTION_PACKAGE_REMOVED)
@@ -17,6 +22,10 @@ class AdsService : Service() {
         val rec = ShowAds()
         registerReceiver(rec, intentFilter)
         MobileAds.initialize(this@AdsService)
-        return TODO("Provide the return value")
+    }
+
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        Log.i("LocalService", "Received start id " + startId + ": " + intent)
+        return START_STICKY
     }
 }
